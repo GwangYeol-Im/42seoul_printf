623ab6007b42209ae3286af6a855c7fb6bc67590
Kyle <mkitigy@gmail.com>
2020-10-28 21:21:13 +0900

    check info, print_nbr 구현

--- print_format.c
+++ print_format.c
@@ -6,7 +6,7 @@
 /*   By: gim <gim@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 12:07:34 by gim               #+#    #+#             */
-/*   Updated: 2020/10/28 20:15:30 by gim              ###   ########.fr       */
+/*   Updated: 2020/10/28 20:58:00 by gim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,29 +18,52 @@ void		init_info(t_info *info)
 	info->nbr_sign = 1;
 }
 
+void		check_info(char *buf, t_info *info)
+{
+	int		idx;
+
+	idx = 0;
+	while (buf[idx])
+	{
+		if (ft_isdigit(buf[idx]))
+		{
+			if (buf[idx] == '0')
+				info->zero = 1;
+			else
+				info->width = buf[idx] - '0';
+		}
+		else if (buf[idx] == '-')
+			info->minus = 1;
+	}
+	if (info->minus == 1)
+		info->zero = 0;
+	info->type = buf[idx - 1];
+}
+
 int			print_format(char *buf, va_list ap)
 {
 	int		len;
 	int		ret;
-	t_info	*Info;
+	t_info	*info;
 
 	ret = 0;
 	len = ft_strlen(buf);
-	Info = (t_info *)malloc(sizeof(t_info));
-	init_info(Info);
-	Info->type = buf[len - 1];
-	if (Info->type == '%')
+	if (!(info = (t_info *)malloc(sizeof(t_info))))
+		return (-1);
+	init_info(info);
+	check_info(buf, info);
+	if (info->type == '%')
 		ret = print_char('%');
-	else if (Info->type == 'c')
+	else if (info->type == 'c')
 		ret = print_char(va_arg(ap, int));
-	else if (Info->type == 's')
+	else if (info->type == 's')
 		ret = print_str(va_arg(ap, char *));
-	else if (Info->type == 'd' || Info->type == 'i')
+	else if (info->type == 'd' || info->type == 'i')
 		ret = print_nbr(va_arg(ap, int));
-	else if (Info->type == 'u' || Info->type == 'x' || Info->type == 'X')
+	else if (info->type == 'u' || info->type == 'x' || info->type == 'X')
 		ret = print_nbr(va_arg(ap, unsigned int));
-	else if (Info->type == 'p')
+	else if (info->type == 'p')
 		ret = print_nbr(va_arg(ap, unsigned long long));
-	free(Info);
+	free(info);
 	return (ret);
 }
