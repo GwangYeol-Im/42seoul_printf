57eb8906749e92d45246a2856660305df6d76713
Kyle <mkitigy@gmail.com>
2020-10-28 21:54:29 +0900

    check_info, check_width_and_prec

--- print_format.c
+++ print_format.c
@@ -6,7 +6,7 @@
 /*   By: gim <gim@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 12:07:34 by gim               #+#    #+#             */
-/*   Updated: 2020/10/28 20:58:00 by gim              ###   ########.fr       */
+/*   Updated: 2020/10/28 21:52:30 by gim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,38 @@
 
 void		init_info(t_info *info)
 {
+	info->minus = 0;
+	info->width = 0;
+	info->zero = 0;
+	info->prec = 0;
 	info->nbr_base = 10;
 	info->nbr_sign = 1;
 }
 
-void		check_info(char *buf, t_info *info)
+void		check_width_and_prec(char c, t_info *info, va_list ap)
+{
+	
+}
+
+void		check_info(char *buf, t_info *info, va_list ap)
 {
 	int		idx;
 
 	idx = 0;
-	while (buf[idx])
+	while (!ft_strrchr(TYPE, buf[idx]))
 	{
-		if (ft_isdigit(buf[idx]))
-		{
-			if (buf[idx] == '0')
-				info->zero = 1;
-			else
-				info->width = buf[idx] - '0';
-		}
+		if (buf[idx] == '0' && info->width == 0 && info->prec == 0)
+			info->zero = 1;
 		else if (buf[idx] == '-')
 			info->minus = 1;
+		else if (buf[idx] == '.')
+			info->prec = 1;
+		else if (ft_isdigit(buf[idx]) || buf[idx] == '*')
+			check_width_and_prec(buf[idx], info, ap);
 	}
+	info->type = buf[idx];
 	if (info->minus == 1)
 		info->zero = 0;
-	info->type = buf[idx - 1];
 }
 
 int			print_format(char *buf, va_list ap)
@@ -51,7 +59,7 @@ int			print_format(char *buf, va_list ap)
 	if (!(info = (t_info *)malloc(sizeof(t_info))))
 		return (-1);
 	init_info(info);
-	check_info(buf, info);
+	check_info(buf, info, ap);
 	if (info->type == '%')
 		ret = print_char('%');
 	else if (info->type == 'c')
